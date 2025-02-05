e4514e1800e37518b11f18168dde6cf535e84e2a
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-05 13:53:07 +0000

    Add indices helper

--- src/lowercase_string.rs
+++ src/lowercase_string.rs
@@ -10,6 +10,10 @@ impl LowercaseString {
                 .collect(),
         )
     }
+
+    fn indices(&self) -> Vec<u8> {
+        self.0.chars().map(|c| (c as u8) - b'a').collect()
+    }
 }
 
 impl AsRef<str> for LowercaseString {
@@ -27,5 +31,15 @@ mod tests {
         assert_eq!(LowercaseString::coerce("Hello123").as_ref(), "hello");
         assert_eq!(LowercaseString::coerce("ABC def!").as_ref(), "abcdef");
         assert_eq!(LowercaseString::coerce("").as_ref(), "");
+    }
+
+    #[test]
+    fn test_indices() {
+        assert_eq!(LowercaseString::coerce("abc").indices(), vec![0, 1, 2]);
+        assert_eq!(
+            LowercaseString::coerce("hello").indices(),
+            vec![7, 4, 11, 11, 14]
+        );
+        assert_eq!(LowercaseString::coerce("").indices(), Vec::<u8>::new());
     }
 }
