2237d64b8101d283972d0d30a7a17be1bbd2e2b7
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-05 13:55:18 +0000

    Add letter counts method

--- src/lowercase_string.rs
+++ src/lowercase_string.rs
@@ -14,6 +14,14 @@ impl LowercaseString {
     fn indices(&self) -> Vec<u8> {
         self.0.chars().map(|c| (c as u8) - b'a').collect()
     }
+
+    pub fn letter_counts(&self) -> [usize; 26] {
+        let mut counts = [0; 26];
+        for idx in self.indices() {
+            counts[idx as usize] += 1;
+        }
+        counts
+    }
 }
 
 impl AsRef<str> for LowercaseString {
@@ -41,5 +49,17 @@ mod tests {
             vec![7, 4, 11, 11, 14]
         );
         assert_eq!(LowercaseString::coerce("").indices(), Vec::<u8>::new());
+    }
+
+    #[test]
+    fn test_letter_counts() {
+        let mut expected = [0; 26];
+        expected[7] = 1; // h
+        expected[4] = 1; // e
+        expected[11] = 2; // l
+        expected[14] = 1; // o
+        assert_eq!(LowercaseString::coerce("hello").letter_counts(), expected);
+
+        assert_eq!(LowercaseString::coerce("").letter_counts(), [0; 26]);
     }
 }
